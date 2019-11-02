01360111d98bb35bfeb631c492795029a8d1e8ae
Fede Castiglione <feder102@gmail.com>
2019-11-02 12:15:44 -0300

    Mis modelitos

--- database/index.js
+++ database/index.js
@@ -1,9 +1,13 @@
 var Datastore = require('nedb')
 
-mascotas = new Datastore({ filename: './database/datastores/mascotas.db', autoload: true })
-vacunas = new Datastore({ filename: './database/datastores/vacunas.db', autoload: true })
+medicos = new Datastore({ filename: './database/datastores/medicos.db', autoload: true })
+pacientes = new Datastore({ filename: './database/datastores/pacientes.db', autoload: true })
+obra_sociales = new Datastore({filename: './database/datastores/obra_sociales.db', autoload: true})
+clinicas = new Datastore ({filename: './database/datastores/clinicas.md', autoload: true})
 
 module.exports = {
-    mascotas,
-    vacunas
+    medicos,
+    pacientes,
+    obra_sociales,
+    clinicas
 }
