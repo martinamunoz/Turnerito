1d51c0c6f9f79a2b03bcda403a5f49523e1efe04
pollo <gonza.fede.sanchez@gmail.com>
2019-11-09 10:12:03 -0300

    yaml documentation

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
 // Importamos los routers
-const mascotasRouter = require('./routes/mascotas.js')
-const vacunasRouter = require('./routes/vacunas.js')
+// const mascotasRouter = require('./routes/mascotas.js')
+// const vacunasRouter = require('./routes/vacunas.js')
 
 // Instanciamos una app de Express
 const app = express()
@@ -17,39 +17,37 @@ const swaggerUi= require('swagger-ui-express')
 const apiDocs = require('./docs/index.js')  
 
 // Utilizamos los middleware
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+// app.use(morgan('dev'))
+// app.use(bodyParser.json())
 // app.use('/headers',require('./routes/headers'));
-app.get('/', (req,res,next) => {
+// app.get('/', (req,res,next) => {
 
-    res.send({
-       nombre: 'API Veterinaria',
-       version: '1.0.0',
-       links: [{
+//     res.send({
+//        nombre: 'API Veterinaria',
+//        version: '1.0.0',
+//        links: [{
    
-               rel:'mascotas',
-               href: '/mascotas'
-           },
-           {
-               rel: 'vacunas',
-               href: '/vacunas'
-           }
-       ]
-       })
-   })
+//                rel:'mascotas',
+//                href: '/mascotas'
+//            },
+//            {
+//                rel: 'vacunas',
+//                href: '/vacunas'
+//            }
+//        ]
+//        })
+//    })
 
-   // Montamos la documentacion en la ruta /docs
+// Montamos la documentacion en la ruta /docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
-app.options('/',(req,res,next) =>{
-
-    
+app.options('/',(req,res,next) =>{    
     res.header('allow', 'GET')
     .status(204)
     .send()
 })
 // Le indicamos a nuestra app que use los routers
-app.use('/mascotas', mascotasRouter)
-app.use('/vacunas', vacunasRouter)
+// app.use('/mascotas', mascotasRouter)
+// app.use('/vacunas', vacunasRouter)
 
 // Establecemos el middleware para manejo de error 404
 app.use((req, res, next) => {
